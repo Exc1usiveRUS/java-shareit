3c0214c6dada79c0de895bae980307916b2981a6
Exc1usiveRUS <shark100494@yandex.ru>
2025-02-07 01:47:59 +0300

    ФЗ 16 сейв

---
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.booking.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class BookingDto {
+    private Long id;
+    private Long itemId;
+    private Item item;
+    private User booker;
+    private BookingStatus status;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+}
